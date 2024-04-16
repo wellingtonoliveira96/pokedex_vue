8b5c8fc0df9c621cd3d852659839e586d3d0eab8
wellington.p <weopereira@sp.gov.br>
2024-04-16 12:16:58 -0300

    mais alterações de estilo na home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,22 @@
 
 <template>
   <main>
-    Home
+    <div class="container text-center">
+      <div class="row mt-4">
+        <div class="col-sm-12 col-md-6">
+          <div class="card" style="width: 18rem;">
+            <img src="https://assets.pokemon.com/assets/cms2/img/misc/countries/pt/country_detail_pokemon.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6">
+          Column
+        </div>
+      </div>
+    </div>
   </main>
 </template>
