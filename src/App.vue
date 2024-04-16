5dd4c723f77d546eea0f62fafbfedc4f4bff0803
wellington.p <weopereira@sp.gov.br>
2024-04-16 11:41:16 -0300

    subindo projeto com adição de bootstrap e primeiras configurações

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+
+import {ref} from 'vue'
+import { RouterView } from 'vue-router';
+
+let num = ref(0)
+
+</script>
+
+<template>
+  <!-- NAVBAR -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <router-link class="navbar-brand" to="/">
+      <img src="https://cdn-icons-png.flaticon.com/512/188/188987.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+      Pokevite
+    </router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link" aria-current="page" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/about">About</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- Content -->
+<div class="main">
+    <router-view></router-view>
+</div>
+
+<footer class="bg-primary text-light">
+  <div>Design in {{  new Date().getFullYear() }}</div>
+</footer>
+
+</template>
+
+<style>
+
+footer{
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 56px;
+}
+</style>
